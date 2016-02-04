903b3f30a8fc51aa8b1629d51b70d62151d32d39
Steven Scanlon <mrstevenscanlon@gmail.com>
2016-02-03 22:04:18 -0800

    Local Storage Changes

--- table.js
+++ table.js
@@ -1,56 +1,86 @@
 var $TABLE = $('#table');
-var $BTN = $('#export-btn');
-var $EXPORT = $('#export');
-var theContent = $('#content');
+var index = 0
+
+function getNotes() {
+    var notes = localStorage.getItem("notes")
+    index = parseInt(localStorage.getItem("index"))
+    if ( isNaN(index) ) {
+        index = 0
+    }
+    if ( !notes ) {
+        notes = []
+    }
+    else {
+        notes = JSON.parse(notes)
+    }
+    return notes
+}
+
+function storeNotes( notes, index ) {
+    localStorage.setItem( "notes", JSON.stringify(notes) )
+    localStorage.setItem("index", index.toString())
+}
+
+function removeNote( id ) {
+    var notes = getNotes()
+    var noteId = parseInt(id)
+    for ( var inx = 0; inx < notes.length; inx++ ) {
+        if ( notes[inx].id == noteId ) {
+            notes.splice(inx, 1)
+            break;
+        }
+    }
+    storeNotes(notes,index)
+}
+
+function updateNote( id, newText ) {
+    var notes = getNotes()
+    var noteId = parseInt(id)
+    for ( var inx = 0; inx < notes.length; inx++ ) {
+        if ( notes[inx].id == noteId ) {
+            notes[inx].note = newText
+            break;
+        }
+    }
+    storeNotes(notes,index)
+}
+
+$(document).ready( function() {
+    var notes = getNotes()
+
+    for ( var inx=0; inx < notes.length; inx++ ) {
+        var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+        $clone[0].id = notes[inx].id
+        $clone.find(".note-text").text(notes[inx].note)
+        $TABLE.find('table').append($clone);
+    }
+});
+
+$(".note-text").blur( function() {
+    updateNote( $(this).parents('tr')[0].id, $(this).text())
+});
+
 
 $('.table-add').click(function () {
+    var notes = getNotes()
+    var note = { id: index, note: "New Note"}
+    notes.push(note)
     var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+    $clone[0].id = index
+    storeNotes(notes, ++index)
     $TABLE.find('table').append($clone);
-    
+
 });
 
 $('.table-remove').click(function () {
+    removeNote( $(this).parents('tr')[0].id )
     $(this).parents('tr').detach();
 });
 
-$('.table-up').click(function () {
-    var $row = $(this).parents('tr');
-    if ($row.index() === 1) return; // Don't go above the header
-    $row.prev().before($row.get(0));
-});
+$('.clear-all').click(function () {
+    var notes = getNotes()
+    for ( var inx = 0; inx < index; inx++ ) {
+        removeNote( notes[inx].id )
 
-$('.table-down').click(function () {
-    var $row = $(this).parents('tr');
-    $row.next().after($row.get(0));
-});
-
-// A few jQuery helpers for exporting only
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
-
-$BTN.click(function () {
-    var $rows = $TABLE.find('tr:not(:hidden)');
-    var headers = [];
-    var data = [];
-
-    // Get the headers (add special header logic here)
-    $($rows.shift()).find('th:not(:empty)').each(function () {
-        headers.push($(this).text().toLowerCase());
-    });
-
-    // Turn all existing rows into a loopable array
-    $rows.each(function () {
-        var $td = $(this).find('td');
-        var h = {};
-
-        // Use the headers from earlier to name our hash keys
-        headers.forEach(function (header, i) {
-            h[header] = $td.eq(i).text();
-        });
-
-        data.push(h);
-    });
-
-    // Output the result
-    $EXPORT.text(JSON.stringify(data));
+    }
 });
